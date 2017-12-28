5ee1dc1b7b153b6d5f7466a848bcd4e6ac2c5266
roundrobine <roundrobine@gmail.com>
2017-12-28 02:34:56 +0100

    sort by author and book title, replace deferred loading with infinite scroll

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,84 +1,86 @@
 'use strict';
 (function() {
 
-  var MainCtrl = function ($scope, BooksService, books) {
+  var MainCtrl = function ($scope, BooksService) {
 
     var vm = this;
     const SORT_BY_TITLE = 'name';
     const SORT_BY_AUTHOR_NAME = "author.name";
     vm.propToSortOn = '';
     vm.reverse = false;
+    vm.isLoading = false;
 
     // In this example, we set up our model using a class.
-    // is that we implement getItemAtIndex and getLength.
-    var DynamicItems = function(books) {
-      /**
-       * @type {!Object<?Array>} Data pages, keyed by page number (0-index).
-       */
-      this.loadedPages = {};
+    var DynamicItems = function() {
 
-      /** @type {number} Total number of items. */
-      this.numItems = books.total;
+      this.pageSize = 150;
+      this.waitStep = 135;
+      this.pageNumber = 0;
+      this.numLoaded = 0;
+      this.toWait = 0;
+      this.books = [];
 
-      /** @const {number} Number of items to fetch per request. */
-      this.PAGE_SIZE = 50;
     };
 
-    // Required.
+
+    // we implement the contract of getItemAtIndex function.
     DynamicItems.prototype.getItemAtIndex = function(index) {
-      var pageNumber = Math.floor(index / this.PAGE_SIZE);
-      var page = this.loadedPages[pageNumber];
-
-      if (page) {
-        return page[index % this.PAGE_SIZE];
+      if (index > this.numLoaded) {
+        this.fetchMoreItems(index, vm.propToSortOn);
+        return null;
       }
-      else if (page !== null) {
-        this.fetchPage_(pageNumber);
-      }
+      return this.books[index];
     };
 
-    // Required.
+
+    // we implement the contract of getLength function
+    // For infinite scroll behavior, we always return a slightly higher
+    // number than the previously loaded items.
     DynamicItems.prototype.getLength = function() {
-      return this.numItems;
+      return this.numLoaded + 35;
     };
 
 
-    DynamicItems.prototype.fetchPage_ = function(pageNumber) {
-      // Set the page to null so we know it is already being fetched.
-      this.loadedPages[pageNumber] = null;
-
-      if(books){
-        this.loadedPages[pageNumber] = [];
-        var pageOffset = books.offset;
-        this.loadedPages[pageNumber] = books.docs;
-        books = null;
-      }else {
+    DynamicItems.prototype.fetchMoreItems = function(index, sortBy) {
+      var limit = this.pageSize;
+      var waitStep = this.waitStep;
+      if(sortBy){
+        vm.isLoading = true;
+        limit = 10 * limit;
+        waitStep = 10 * waitStep;
+      }
+      if (this.toWait < index) {
+        this.toWait += waitStep;
+        this.pageNumber += 1;
         BooksService.paged({
-          page: pageNumber +1,
-          limit: this.PAGE_SIZE
-        },
-        function (books) {
-          this.loadedPages[pageNumber] = [];
-          var pageOffset = books.offset;
-          this.loadedPages[pageNumber] = books.docs;
-        }.bind(this));
-     }
-    };
+            page: this.pageNumber,
+            limit: limit,
+            sortBy: sortBy
+          },
+          function (books) {
+            this.books = this.books.concat(books.docs);
+            this.numLoaded = this.toWait;
+            vm.isLoading = false;
+          }.bind(this));
+      }
+    }
 
-    vm.sort = function(keyname){
-      vm.propToSortOn = keyname;
+    vm.sort = function(sortBy){
+      vm.dinamicBooks = null;
+      vm.topIndex = 0;
+      vm.propToSortOn = sortBy;
       vm.reverse = !vm.reverse;
       if(vm.reverse){
         vm.propToSortOn = '-' + vm.propToSortOn;
       }
-      //getResultsPage($scope.currenPage);
+      vm.dinamicBooks = new DynamicItems();
     };
 
-    vm.dinamicBooks = new DynamicItems(books);
+    vm.dinamicBooks = new DynamicItems();
 
   };
 
-  MainCtrl.$inject = ['$scope', 'BooksService', 'books'];
+  MainCtrl.$inject = ['$scope', 'BooksService'];
 
   angular.module('oneMillionBooksApp')
     .controller('MainCtrl', MainCtrl);
